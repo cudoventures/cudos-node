a1595c3c877a80a8450d1da0ce88642721a57496
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-04-28 18:13:40 +0300

    update faucet

--- project-faucet-cli/run-testnet.sh
+++ project-faucet-cli/run-testnet.sh
@@ -6,6 +6,6 @@ export KEYRING_BACKEND="test"
 export CREDIT_AMOUNT="1000"
 export MAX_CREDIT="100000000"
 export NODE="http://10.128.0.2:26657"
-export MNEMONIC="hat festival protect hen timber erupt crash stand kind slush cheese access boil consider blur glide chimney tribe cherry grass wide ship pizza resist"
+export MNEMONIC="become bind merge kitchen oval chair erase material draw impose enemy useful habit science project face multiply bird pact bottom ahead basic kitten boy"
 
 faucet
