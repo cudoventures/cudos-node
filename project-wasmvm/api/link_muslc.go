4e388ae4c36defe78d7312f39810b2d82556653e
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-05-13 15:01:10 +0300

    remove wasmvm src

--- project-wasmvm/api/link_muslc.go
+++
@@ -1,6 +0,0 @@
-// +build linux,muslc
-
-package api
-
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lwasmvm_muslc
-import "C"
--- project-wasmvm/builders/guest/build_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cargo build --release
-cp target/release/libwasmvm.a api
-# FIXME: re-enable stripped so when we approach a production release, symbols are nice for debugging
-# strip api/libwasmvm.so
