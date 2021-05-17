1cf2c6d8b4fa2ae87559a6d144bb70787d3296e3
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-05-17 17:18:39 +0300

    refactor

--- project-explorer/both/utils/time.js
+++
@@ -1,4 +0,0 @@
-export const goTimeToISOString = (time) => {
-    const millisecond = parseInt(time.seconds+time.nanos.toString().substring(0,3));
-    return (new Date(millisecond)).toISOString()
-}
--- project-explorer/scripts/genversion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Generating version file"
-export TAG=$(git describe --tag)
-echo $TAG | tee private/version
-git add private/version
-git commit -am "add version $TAG"
