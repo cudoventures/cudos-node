3d74bd74cf37b873975082eb37ab696a8a16653a
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-05-18 11:26:52 +0300

    update gitignore

---
+++ cmd/cudos-noded/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"cudos.org/cudos-node/app"
+	"cudos.org/cudos-node/cmd/cudos-noded/cmd"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+)
+
+func main() {
+	app.SetConfig()
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+		os.Exit(1)
+	}
+}
