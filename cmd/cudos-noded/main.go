70eb042ec56f7ac919d2afb73022c8c56c90a52d
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-08-20 19:15:21 +0300

    update power reduction to 10^18

--- cmd/cudos-noded/main.go
+++ cmd/cudos-noded/main.go
@@ -6,9 +6,11 @@ import (
 	"cudos.org/cudos-node/app"
 	"cudos.org/cudos-node/cmd/cudos-noded/cmd"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func main() {
+	sdk.DefaultPowerReduction = sdk.NewIntFromUint64(100000000000000000)
 	app.SetConfig()
 	rootCmd, _ := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
