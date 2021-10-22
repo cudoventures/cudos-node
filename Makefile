c16f51baf134b921ae0d50012f8f302be6b53f2b
infra1515 <lyubomir.yonchev@limechain.tech>
2021-10-22 15:33:02 +0300

    [CUDOS-249]Makefile altered. Simulation tests added. Units tests added - MintNFT fully tested. Other functions coming soon.

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
 PACKAGES=$(shell go list ./... | grep -v '/simulation')
+PACKAGES_UNITTEST=$(shell go list ./... | grep -v '/simulation' | grep -v '/cli_test')
+PACKAGES_SIMTEST=$(shell go list ./... | grep '/simulation')
+SIMAPP = ./simapp
+BINDIR ?= $(GOPATH)/bin
+
 
 VERSION := $(shell echo $(shell git describe --tags) | sed 's/^v//')
 COMMIT := $(shell git log -1 --format='%H')
@@ -10,7 +15,7 @@ ldflags = -X github.com/cosmos/cosmos-sdk/version.Name=cudos-node \
 
 BUILD_FLAGS := -ldflags '$(ldflags)'
 
-all: install
+all: install test
 
 install: go.sum
 		@echo "--> Installing cudos-noded"
@@ -20,5 +25,13 @@ go.sum: go.mod
 		@echo "--> Ensure dependencies have not been modified"
 		GO111MODULE=on go mod verify
 
-test:
-	@go test -mod=readonly $(PACKAGES)
+
+test: test-unit
+
+test-unit:
+	@VERSION=$(VERSION) go test -mod=readonly -ldflags '$(ldflags)' ${PACKAGES_UNITTEST}
+
+test-sim-nondeterminism-fast:
+	@echo "Running non-determinism test..."
+	@go test -mod=readonly $(SIMAPP) -run TestAppStateDeterminism -Enabled=true \
+		-NumBlocks=10 -BlockSize=200 -Commit=true -Period=0 -v -timeout 24h
--- x/nft/keeper/keeper_test.go
+++ x/nft/keeper/keeper_test.go
@@ -1,1 +1,215 @@
-package keeper
+package keeper_test
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"cudos.org/cudos-node/simapp"
+	"cudos.org/cudos-node/x/nft/keeper"
+	"cudos.org/cudos-node/x/nft/types"
+)
+
+var (
+	denomID     = "denomid"
+	denomNm     = "denomnm"
+	denomSymbol = "denomSymbol"
+	schema      = "{a:a,b:b}"
+
+	denomID2     = "denomid2"
+	denomNm2     = "denom2nm"
+	denomSymbol2 = "denomSymbol2"
+
+	tokenID  = "tokenid"
+	tokenID2 = "tokenid2"
+	tokenID3 = "tokenid3"
+
+	tokenNm  = "tokennm"
+	tokenNm2 = "tokennm2"
+	tokenNm3 = "tokennm3"
+
+	denomID3     = "denomid3"
+	denomNm3     = "denom3nm"
+	denomSymbol3 = "denomSymbol3"
+
+	address   = CreateTestAddrs(1)[0]
+	address2  = CreateTestAddrs(2)[1]
+	address3  = CreateTestAddrs(3)[2]
+	tokenURI  = "https://google.com/token-1.json"
+	tokenURI2 = "https://google.com/token-2.json"
+	tokenData = "{a:a,b:b}"
+
+	isCheckTx = false
+)
+
+type KeeperSuite struct {
+	suite.Suite
+
+	legacyAmino *codec.LegacyAmino
+	ctx         sdk.Context
+	keeper      keeper.Keeper
+	app         *simapp.SimApp
+
+	queryClient types.QueryClient
+}
+
+type MockedKeeper struct {
+	mock.Mock
+}
+
+func (suite *KeeperSuite) SetupTest() {
+
+	app := simapp.Setup(isCheckTx)
+
+	suite.app = app
+	suite.legacyAmino = app.LegacyAmino()
+	suite.ctx = app.BaseApp.NewContext(isCheckTx, tmproto.Header{})
+	suite.keeper = app.NftKeeper
+
+	queryHelper := baseapp.NewQueryServerTestHelper(suite.ctx, app.InterfaceRegistry())
+	types.RegisterQueryServer(queryHelper, app.NftKeeper)
+	suite.queryClient = types.NewQueryClient(queryHelper)
+
+}
+
+func (suite *KeeperSuite) AfterTest(_, _ string) {
+}
+
+//TODO: Refactor with Should syntaxis
+func (suite *KeeperSuite) TestIssueDenom() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address)
+	suite.NoError(err)
+
+	// MintNFT shouldn't fail when collection does not exist
+	err = suite.keeper.IssueDenom(suite.ctx, denomID2, denomNm2, schema, address)
+	suite.NoError(err)
+
+	err = suite.keeper.IssueDenom(suite.ctx, denomID3, denomNm3, schema, address3)
+	suite.NoError(err)
+
+	// collections should equal 3
+	collections := suite.keeper.GetCollections(suite.ctx)
+	suite.NotEmpty(collections)
+	suite.Equal(len(collections), 3)
+}
+
+func (suite *KeeperSuite) TestMintNFT_ShouldError_WhenSenderIsNotDenomCreator() {
+
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, denomNm, tokenURI, tokenData, address2, address)
+	suite.ErrorIs(err, types.ErrUnauthorized)
+}
+
+func (suite *KeeperSuite) TestMintNFT_ShouldError_WhenDenomDoesNotExist() {
+	err := suite.keeper.MintNFT(suite.ctx, denomID, tokenID, denomNm, tokenURI, tokenData, address2, address)
+	suite.ErrorIs(err, types.ErrInvalidDenom)
+}
+
+func (suite *KeeperSuite) TestMintNFT_ShouldError_WhenNFTAlreadyExists() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, denomNm, tokenURI, tokenData, address, address2)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, denomNm, tokenURI, tokenData, address, address2)
+	suite.ErrorIs(err, types.ErrNFTAlreadyExists)
+
+}
+
+func (suite *KeeperSuite) TestMintNFT_Correctly_MintsNewNFT() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, denomNm, tokenURI, tokenData, address, address2)
+	suite.NoError(err)
+
+	nftSuccessfullyMinted := suite.keeper.HasNFT(suite.ctx, denomID, tokenID)
+	assert.Equal(suite.T(), true, nftSuccessfullyMinted)
+
+}
+
+func (suite *KeeperSuite) TestMintNFT_Correctly_SetsOwner() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, denomNm, tokenURI, tokenData, address, address2)
+	suite.NoError(err)
+
+	nft, err := suite.keeper.GetBaseNFT(suite.ctx, denomID, tokenID)
+	suite.NoError(err)
+
+	assert.Equal(suite.T(), nft.Owner, address2.String())
+
+}
+
+func (suite *KeeperSuite) TestMintNFT_Correctly_IncreasesTotalSupply() {
+	supplyBeforeMinting := suite.keeper.GetTotalSupply(suite.ctx, denomID)
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, denomNm, tokenURI, tokenData, address, address2)
+	suite.NoError(err)
+	supplyAfterMinting := suite.keeper.GetTotalSupply(suite.ctx, denomID)
+
+	assert.Greater(suite.T(), supplyAfterMinting, supplyBeforeMinting)
+
+}
+
+func TestKeeperSuite(t *testing.T) {
+	suite.Run(t, new(KeeperSuite))
+}
+
+// CreateTestAddrs creates test addresses
+func CreateTestAddrs(numAddrs int) []sdk.AccAddress {
+	var addresses []sdk.AccAddress
+	var buffer bytes.Buffer
+
+	// start at 100 so we can make up to 999 test addresses with valid test addresses
+	for i := 100; i < (numAddrs + 100); i++ {
+		numString := strconv.Itoa(i)
+		buffer.WriteString("A58856F0FD53BF058B4909A21AEC019107BA6") //base address string
+
+		buffer.WriteString(numString) //adding on final two digits to make addresses unique
+		res, _ := sdk.AccAddressFromHex(buffer.String())
+		bech := res.String()
+		addresses = append(addresses, testAddr(buffer.String(), bech))
+		buffer.Reset()
+	}
+
+	return addresses
+}
+
+// for incode address generation
+func testAddr(addr string, bech string) sdk.AccAddress {
+	res, err := sdk.AccAddressFromHex(addr)
+	if err != nil {
+		panic(err)
+	}
+	bechexpected := res.String()
+	if bech != bechexpected {
+		panic("Bech encoding doesn't match reference")
+	}
+
+	bechres, err := sdk.AccAddressFromBech32(bech)
+	if err != nil {
+		panic(err)
+	}
+	if !bytes.Equal(bechres, res) {
+		panic("Bech decode and hex decode don't match")
+	}
+
+	return res
+}
