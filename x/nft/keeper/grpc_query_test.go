f5df840e4286884126bcb20c338ea4ba5e840ed6
infra1515 <lyubomir.yonchev@limechain.tech>
2021-10-25 18:11:41 +0300

    [CUDOS-249] Tests added for grpc_query

---
+++ x/nft/keeper/grpc_query_test.go
@@ -0,0 +1,136 @@
+package keeper_test
+
+import (
+	gocontext "context"
+	"cudos.org/cudos-node/x/nft/types"
+)
+
+func (suite *IntegrationTestKeeperSuite) TestSupplyReturnsCorrect() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address2)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address2, address)
+	suite.NoError(err)
+
+	response, err := suite.queryClient.Supply(gocontext.Background(), &types.QuerySupplyRequest{
+		DenomId: denomID,
+		Owner:   address.String(),
+	})
+
+	suite.NoError(err)
+	suite.Equal(1, int(response.Amount))
+}
+
+func (suite *IntegrationTestKeeperSuite) TestOwner_ReturnsCorrect() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address2)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address2, address)
+	suite.NoError(err)
+
+	response, err := suite.queryClient.Owner(gocontext.Background(), &types.QueryOwnerRequest{
+		DenomId: denomID,
+		Owner:   address.String(),
+	})
+
+	suite.NoError(err)
+	suite.NotNil(response.Owner)
+	suite.Contains(response.Owner.IDCollections[0].TokenIds, tokenID)
+}
+
+func (suite *IntegrationTestKeeperSuite) TestCollection_ReturnsCorrect() {
+
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address2)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address2, address)
+	suite.NoError(err)
+
+	response, err := suite.queryClient.Collection(gocontext.Background(), &types.QueryCollectionRequest{
+		DenomId: denomID,
+	})
+
+	suite.NoError(err)
+	suite.NotNil(response.Collection)
+	suite.Len(response.Collection.NFTs, 1)
+	suite.Equal(response.Collection.NFTs[0].Id, tokenID)
+}
+
+func (suite *IntegrationTestKeeperSuite) TestDenom_ReturnsCorrect() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address2)
+	suite.NoError(err)
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address2, address)
+	suite.NoError(err)
+
+	response, err := suite.queryClient.Denom(gocontext.Background(), &types.QueryDenomRequest{
+		DenomId: denomID,
+	})
+
+	suite.NoError(err)
+	suite.NotNil(response.Denom)
+	suite.Equal(response.Denom.Id, denomID)
+}
+
+func (suite *IntegrationTestKeeperSuite) TestDenoms_ReturnsCorrectCollection() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address2)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address2, address)
+	suite.NoError(err)
+
+	response, err := suite.queryClient.Denoms(gocontext.Background(), &types.QueryDenomsRequest{})
+
+	suite.NoError(err)
+	suite.NotEmpty(response.Denoms)
+	suite.Equal(response.Denoms[0].Id, denomID)
+}
+
+func (suite *IntegrationTestKeeperSuite) TestNFT_ReturnsCorrect() {
+
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address2)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address2, address)
+	suite.NoError(err)
+
+	response, err := suite.queryClient.NFT(gocontext.Background(), &types.QueryNFTRequest{
+		DenomId: denomID,
+		TokenId: tokenID,
+	})
+
+	suite.NoError(err)
+	suite.NotEmpty(response.NFT)
+	suite.Equal(response.NFT.Id, tokenID)
+}
+
+func (suite *IntegrationTestKeeperSuite) TestGetApprovalsNFT_Correctly_ReturnsApprovals() {
+	err := suite.keeper.IssueDenom(suite.ctx, denomID, denomNm, schema, address2)
+	suite.NoError(err)
+
+	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address2, address)
+	suite.NoError(err)
+
+	err = suite.keeper.AddApproval(suite.ctx, denomID, tokenID, address, address2)
+	suite.NoError(err)
+
+	response, err := suite.queryClient.GetApprovalsNFT(gocontext.Background(), &types.QueryApprovalsNFTRequest{
+		DenomId: denomID,
+		TokenId: tokenID,
+	})
+
+	suite.NoError(err)
+	suite.Equal(response.ApprovedAddresses[address2.String()], true)
+}
+
+func (suite *IntegrationTestKeeperSuite) TestIsApprovedOperator_ReturnsTrue_WhenOperatorIsApproved() {
+	err := suite.keeper.AddApprovalForAll(suite.ctx, address, address2, true)
+	suite.NoError(err)
+
+	response, err := suite.queryClient.QueryApprovalsIsApprovedForAll(gocontext.Background(), &types.QueryApprovalsIsApprovedForAllRequest{
+		Owner:    address.String(),
+		Operator: address2.String(),
+	})
+
+	suite.NoError(err)
+	suite.Equal(response.IsApproved, true)
+}
