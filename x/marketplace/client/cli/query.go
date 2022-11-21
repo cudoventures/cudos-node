87d05937a4da6ee2fb81db2722ae0f8cb5ff6735
avalkov <avalkov@mail.bg>
2022-11-21 15:59:09 +0200

    Added query-admins

--- x/marketplace/client/cli/query.go
+++ x/marketplace/client/cli/query.go
@@ -31,6 +31,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowNft())
 	cmd.AddCommand(CmdCollectionByDenomId())
 
+	cmd.AddCommand(CmdListAdmins())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
