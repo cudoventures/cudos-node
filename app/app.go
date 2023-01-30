739b0d29ae3657ac066f5af23f56f593d3f5f95d
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-01-30 12:44:36 +0000

    remove send creation of app configurator

--- app/app.go
+++ app/app.go
@@ -307,7 +307,7 @@ func New(
 
 	app.mm.RegisterInvariants(&app.CrisisKeeper)
 	app.mm.RegisterRoutes(app.Router(), app.QueryRouter(), encodingConfig.Amino)
-	app.mm.RegisterServices(module.NewConfigurator(app.appCodec, app.MsgServiceRouter(), app.GRPCQueryRouter()))
+	app.mm.RegisterServices(app.configurator)
 
 	anteHandler, err := ante.NewAnteHandler(
 		ante.HandlerOptions{
