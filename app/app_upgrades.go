7f68a9aaedb57d9aea5820e1793b2b57260306a3
Kamen Stoykov <k.stoykov@razorlabs.com>
2023-01-30 09:59:45 +0000

    add upgrade handlers

--- app/app_upgrades.go
+++ app/app_upgrades.go
@@ -1,6 +1,11 @@
 package app
 
 import (
+	"strings"
+
+	addressbookTypes "github.com/CudoVentures/cudos-node/x/addressbook/types"
+	cudoMinttypes "github.com/CudoVentures/cudos-node/x/cudoMint/types"
+	marketplaceTypes "github.com/CudoVentures/cudos-node/x/marketplace/types"
 	nfttypes "github.com/CudoVentures/cudos-node/x/nft/types"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,11 +17,30 @@ import (
 )
 
 func (app *App) SetUpgradeHandlers() {
-	setV110Handler(app)
+	setHandlerForVersion_1_0(app)
+	setHandlerForVersion_1_1(app)
 }
 
-func setV110Handler(app *App) {
-	const upgradeVersion string = "v1.1.0"
+func setHandlerForVersion_1_0(app *App) {
+	app.UpgradeKeeper.SetUpgradeHandler("v1.0", func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		ss, ok := app.ParamsKeeper.GetSubspace(cudoMinttypes.ModuleName)
+		if ok {
+			bpd := ss.GetRaw(ctx, []byte("BlocksPerDay"))
+
+			bpdString := strings.ReplaceAll(string(bpd), "\"", "")
+
+			bpdInt, parseOk := sdk.NewIntFromString(bpdString)
+			if parseOk {
+				ss.Set(ctx, []byte("IncrementModifier"), bpdInt)
+			}
+		}
+
+		return fromVM, nil
+	})
+}
+
+func setHandlerForVersion_1_1(app *App) {
+	const upgradeVersion string = "v1.1"
 
 	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		if len(fromVM) == 0 {
@@ -43,7 +67,7 @@ func setV110Handler(app *App) {
 
 	if upgradeInfo.Name == upgradeVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := storetypes.StoreUpgrades{
-			Added: []string{authz.ModuleName, group.ModuleName},
+			Added: []string{authz.ModuleName, group.ModuleName, addressbookTypes.ModuleName, marketplaceTypes.ModuleName},
 		}
 
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
