46215ec1dd2e9353e35a719e4e63d373fa38b946
TuanTran <tuantran@notional.ventures>
2023-12-12 17:38:26 +0700

    fix path error

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -8,7 +8,8 @@ set -eo pipefail
 go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 
 echo "Generating gogo proto code"
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+cd proto
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep go_package "$file" ; then
@@ -17,6 +18,7 @@ for dir in $proto_dirs; do
   done
 done
 
+cd ..
 
 # move proto files to the right places
 cp -r github.com/CudoVentures/cudos-node/* ./
