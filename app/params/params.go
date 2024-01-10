735776a2d5463e03b2d413ec2cc9d1249a5336ec
dao-notional <dao@notional.ventures>
2024-01-10 16:46:47 +0700

    add apptesting

---
+++ app/params/params.go
@@ -0,0 +1,8 @@
+package params
+
+// Simulation parameter constant
+const (
+	StakePerAccount           = "stake_per_account"
+	InitiallyBondedValidators = "initially_bonded_validators"
+	BondDenom                 = "acudos"
+)
