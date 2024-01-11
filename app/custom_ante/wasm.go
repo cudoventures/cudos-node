a559d5094869a333dea24af2b6084d206a0c1f97
Nathan Baum <n@p12a.org.uk>
2024-01-11 18:49:51 +0000

    hotfix: disable wasm.MsgStoreCode

---
+++ app/custom_ante/wasm.go
@@ -0,0 +1,21 @@
+package custom_ante
+
+import (
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+func DenyMsgStoreCode(next sdk.AnteHandler) sdk.AnteHandler {
+	return func(ctx sdk.Context, tx sdk.Tx, simulate bool) (_ sdk.Context, _ error) {
+		for _, msg := range tx.GetMsgs() {
+			if _, ok := msg.(*wasmtypes.MsgStoreCode); ok {
+				return ctx, sdkerrors.Wrap(
+					sdkerrors.ErrInvalidRequest,
+					"MsgStoreCode is disabled",
+				)
+			}
+		}
+		return next(ctx, tx, simulate)
+	}
+}
