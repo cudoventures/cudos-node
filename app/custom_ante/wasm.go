a8782e67c123f10f674ee198e00022e100f226bf
Nathan Baum <n@p12a.org.uk>
2024-01-12 13:23:15 +0000

    Delay blocking contracts until a given height

--- app/custom_ante/wasm.go
+++ app/custom_ante/wasm.go
@@ -6,14 +6,19 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+// TODO: Replace with the correct height
+const DenyMsgStoreCodeAfter = 1000000
+
 func DenyMsgStoreCode(next sdk.AnteHandler) sdk.AnteHandler {
 	return func(ctx sdk.Context, tx sdk.Tx, simulate bool) (_ sdk.Context, _ error) {
-		for _, msg := range tx.GetMsgs() {
-			if _, ok := msg.(*wasmtypes.MsgStoreCode); ok {
-				return ctx, sdkerrors.Wrap(
-					sdkerrors.ErrInvalidRequest,
-					"MsgStoreCode is disabled",
-				)
+		if ctx.BlockHeight() > DenyMsgStoreCodeAfter {
+			for _, msg := range tx.GetMsgs() {
+				if _, ok := msg.(*wasmtypes.MsgStoreCode); ok {
+					return ctx, sdkerrors.Wrap(
+						sdkerrors.ErrInvalidRequest,
+						"MsgStoreCode is disabled",
+					)
+				}
 			}
 		}
 		return next(ctx, tx, simulate)
