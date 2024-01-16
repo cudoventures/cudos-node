23d2ab560a9d11447ed0d5a66f737b878e2dc2d6
Tuan Tran <tuantran@notional.ventures>
2024-01-16 21:01:32 +0700

    refactor: revert register changes

--- x/bank/module.go
+++ x/bank/module.go
@@ -1,9 +1,12 @@
 package bank
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	bankmodule "github.com/cosmos/cosmos-sdk/x/bank"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	"github.com/cosmos/cosmos-sdk/x/bank/types"
 
 	custombankkeeper "github.com/CudoVentures/cudos-node/x/bank/keeper"
@@ -28,6 +31,10 @@ func NewAppModule(cdc codec.Codec, keeper custombankkeeper.Keeper, accountKeeper
 // NOTE: Overriding this method as not doing so will cause a panic
 // when trying to force this custom keeper into a bankkeeper.BaseKeeper
 func (am AppModule) RegisterServices(cfg module.Configurator) {
-	am.AppModule.RegisterServices(cfg)
-
+	types.RegisterMsgServer(cfg.MsgServer(), bankkeeper.NewMsgServerImpl(am.keeper))
+	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+	m := bankkeeper.NewMigrator(am.keeper.BaseKeeper)
+	if err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2); err != nil {
+		panic(fmt.Sprintf("failed to migrate x/bank from version 1 to 2: %v", err))
+	}
 }
