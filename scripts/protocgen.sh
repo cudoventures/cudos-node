f763a8aa3f27ce6fb5161b8170d5b003db593702
Tuan Tran <tuantran@notional.ventures>
2024-01-29 15:34:14 +0700

    feat: add proto build system

---
+++ scripts/protocgen.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# see pre-requests:
+# - https://grpc.io/docs/languages/go/quickstart/
+# - gocosmos plugin is automatically installed during scaffolding.
+
+set -eo pipefail
+go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
+
+echo "Generating gogo proto code"
+cd proto
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package "$file" ; then
+      buf generate --template buf.gen.gogo.yaml "$file"
+    fi
+  done
+done
+
+cd ..
+
+# move proto files to the right places
+cp -r github.com/CudoVentures/cudos-node/* ./
+rm -rf github.com
+
