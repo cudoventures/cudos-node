cc6283d9b892050ff4be6bdb1c92ec93716d38be
beb4now <33292945+beb4now@users.noreply.github.com>
2024-07-10 12:03:02 +0100

    Update app_upgrades.go
    added 1.2.5

--- app/app_upgrades.go
+++ app/app_upgrades.go
@@ -24,6 +24,7 @@ func (app *App) SetUpgradeHandlers() {
 	setHandlerForVersion_1_2_2(app)
 	setHandlerForVersion_1_2_3(app)
 	setHandlerForVersion_1_2_4(app)
+	setHandlerForVersion_1_2_5(app)
 }
 
 func setHandlerForVersion_1_0(app *App) {
@@ -164,6 +165,32 @@ func setHandlerForVersion_1_2_3(app *App) {
 }
 func setHandlerForVersion_1_2_4(app *App) {
 	const upgradeVersion string = "v1.2.4"
+
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+
+	if upgradeInfo.Name == upgradeVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{},
+			Deleted: []string{
+				AddressBookModuleName,
+				MarketplaceModuleName,
+				NftModuleName,
+				GroupModuleName,
+			},
+		}
+
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+}
+func setHandlerForVersion_1_2_5(app *App) {
+	const upgradeVersion string = "v1.2.5"
 
 	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
