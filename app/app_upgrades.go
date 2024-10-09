eaa579bdd4c8e2cb6cad5aae1f4cb9fe4da8f4ec
jgtormo <joan.garciaitormo@cudoventures.com>
2024-10-09 12:41:46 +0100

    add testnet upgrade handlers back

--- app/app_upgrades.go
+++ app/app_upgrades.go
@@ -21,7 +21,12 @@ const GroupModuleName = "group"
 func (app *App) SetUpgradeHandlers() {
 	setHandlerForVersion_1_0(app)
 	setHandlerForVersion_1_1(app)
-	setHandlerForVersion_1_9_9(app)
+	setHandlerForVersion_1_2(app)
+	setHandlerForVersion_1_2_2(app)
+	setHandlerForVersion_1_2_3(app)
+	setHandlerForVersion_1_2_4(app)
+	setHandlerForVersion_1_2_5(app)
+	setHandlerForVersion_9_1_1(app)
 }
 
 func setHandlerForVersion_1_0(app *App) {
@@ -80,8 +85,124 @@ func setHandlerForVersion_1_1(app *App) {
 	}
 }
 
-func setHandlerForVersion_1_9_9(app *App) {
-	const upgradeVersion string = "v1.9.9"
+func setHandlerForVersion_1_2(app *App) {
+	const upgradeVersion string = "Plan of maximum success"
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+	if upgradeInfo.Name == upgradeVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{},
+			Deleted: []string{
+				AddressBookModuleName,
+				MarketplaceModuleName,
+				NftModuleName,
+				GroupModuleName,
+			},
+		}
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+}
+func setHandlerForVersion_1_2_2(app *App) {
+	const upgradeVersion string = "v1.2.2"
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+	if upgradeInfo.Name == upgradeVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{},
+			Deleted: []string{
+				AddressBookModuleName,
+				MarketplaceModuleName,
+				NftModuleName,
+				GroupModuleName,
+			},
+		}
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+}
+func setHandlerForVersion_1_2_3(app *App) {
+	const upgradeVersion string = "v1.2.3"
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+	if upgradeInfo.Name == upgradeVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{},
+			Deleted: []string{
+				AddressBookModuleName,
+				MarketplaceModuleName,
+				NftModuleName,
+				GroupModuleName,
+			},
+		}
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+}
+func setHandlerForVersion_1_2_4(app *App) {
+	const upgradeVersion string = "v1.2.4"
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+	if upgradeInfo.Name == upgradeVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{},
+			Deleted: []string{
+				AddressBookModuleName,
+				MarketplaceModuleName,
+				NftModuleName,
+				GroupModuleName,
+			},
+		}
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+}
+
+func setHandlerForVersion_1_2_5(app *App) {
+	const upgradeVersion string = "v1.2.5"
+
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+
+	if upgradeInfo.Name == upgradeVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{},
+			Deleted: []string{
+				AddressBookModuleName,
+				MarketplaceModuleName,
+				NftModuleName,
+				GroupModuleName,
+			},
+		}
+
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+}
+
+func setHandlerForVersion_9_1_1(app *App) {
+	const upgradeVersion string = "v9.1.1"
 
 	app.UpgradeKeeper.SetUpgradeHandler(upgradeVersion, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
